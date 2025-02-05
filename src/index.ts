2263a6abcc58b81c84e6807f361ee96fb1efb6e7
Adpro <101449500+adproqwq@users.noreply.github.com>
2025-02-05 21:11:33 +0800

    fix: 导出存档类型

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import Data from './api/Data';
 import Reality from './api/Reality';
+import {
+  ISave,
+  ISaveAgreement,
+  ISaveChapterSelectPreference,
+  ISaveImmerseProgresse,
+  ISaveMarkRecord,
+  ISaveSongRecord,
+} from './types/saves';
 
 export {
   Data,
   Reality,
+};
+
+export {
+  ISave,
+  ISaveAgreement,
+  ISaveChapterSelectPreference,
+  ISaveImmerseProgresse,
+  ISaveMarkRecord,
+  ISaveSongRecord,
 };
